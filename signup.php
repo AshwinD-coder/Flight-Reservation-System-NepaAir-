01120c2a5e6f4830f8fa5ac0fd3e160939801fc3
AshishLawaju <ashish.lawaju74@gmail.com>
2021-07-08 18:07:18 +0545

    Update signup.php

--- signup.php
+++ signup.php
@@ -42,7 +42,8 @@ include('partials/navbar.php');
                         <input type="checkbox" class="custom-control-input" id="customCheck1">
                         <label class="custom-control-label" for="customCheck1">Remember password</label>
                       </div>
-                      <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                      <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button><br><hr>
+                      <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign up as new user</button>
                       <hr class="my-4">
                       <button class="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i class="fab fa-google mr-2"></i> Sign in with Google</button>
                       <button class="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i class="fab fa-facebook-f mr-2"></i> Sign in with Facebook</button>
