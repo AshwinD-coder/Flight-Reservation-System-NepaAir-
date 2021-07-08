79d5807276533b05672633577df1e330466adeb8
Uttam66 <87117937+Uttam66@users.noreply.github.com>
2021-07-08 19:26:16 +0545

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,68 +1,37 @@
-
-<div class='container-fluid'>
-    <div class="card">
-        <div class="heading text-center">
-            <div class="head1">Just Scratching the Surface</div>
-            <p class="bdr"></p>
-        </div>
-        <div class="card-body">
-            <div class="row m-0 pt-5">
-                <div class="card col-12 col-md-3">
-                    <div class="card-content"> <i class="fas fa-hand-holding-usd fa-3x"></i>
-                        <div class="card-title"> RECHARGE REPEAT CUSTOMER </div>
-                        <p><small>Save customers' cards for future phone orders</small></p>
-                        <div class="learn-more">
-                            <p> <small> LEARN MORE <label><i class="fas fa-angle-right"></i></label> </small> </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="card col-12 col-md-3">
-                    <div class="card-content"> <i class="far fa-handshake fa-3x"></i>
-                        <div class="card-title"> ACCEPT ELECTRONIC CHECK </div>
-                        <p><small>Save customers' cards for future phone orders</small></p>
-                        <div class="learn-more">
-                            <p> <small> LEARN MORE <label><i class="fas fa-angle-right"></i></label> </small> </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="card col-12 col-md-3">
-                    <div class="card-content"> <i class="fas fa-mobile-alt fa-3x"></i>
-                        <div class="card-title"> STREAMLINE PHONE PAYMENTS </div>
-                        <p><small>Save customers' cards for future phone orders</small></p>
-                        <div class="learn-more">
-                            <p> <small> LEARN MORE <label><i class="fas fa-angle-right"></i></label> </small> </p>
-                        </div>
-                    </div>
-                </div>
-                <div class="card col-12 col-md-3">
-                    <div class="card-content"> <i class="fas fa-user-secret fa-3x"></i>
-                        <div class="card-title"> STAY<br /> SECURE </div>
-                        <p><small>Save customers' cards for future phone orders</small></p>
-                        <div class="learn-more">
-                            <p> <small> LEARN MORE <label><i class="fas fa-angle-right"></i></label> </small> </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="card-footer row m-0">
-            <p> <label> <i class="fas fa-phone fa-rotate-90 text-primary"></i> </label> 800-601-0230</p>
-            <div>
-                <p> <small class="follow-text">FOLLOW US ON SOCIAL MEDIA</small> <label class="footer-right"> <i class="fab fa-instagram"></i> <i class="fab fa-facebook-square"></i> <i class="fab fa-linkedin"></i> <i class="fab fa-twitter-square"></i> </label> </p>
-            </div>
-        </div>
-    </div>
-</div>
-    
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
-    <script src="bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
-
-
-
-
-  </body>
-</html>
+<div class="footer">
+		<div class="footer-content">
+		<div class="footer-section about">
+		<h1 class="logo-text"><span>NEPA</span>AIR</h1>
+		<p>This is a online website for the User who is seeking to 
+		Travel from one place to another
+		</p><br><br><br><br>
+		<div class="contact">
+		<span><i class="fas fa-phone"></i>&nbsp; 98603478</span>
+		<span><i class="fas fa-envelope-square"></i>&nbsp; nepaair@gmail.com</span>
+		</div>
+		<div class="socials">
+			<a href="www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+			<a href="www.instagram.com"><i class="fab fa-instagram"></i></a>
+			<a href="www.twitter.com"><i class="fab fa-twitter"></i></a>
+		</div>
+		</div>
+		
+		<div class="footer-section links">
+		<h2>Quick links</h2>
+		<br>
+		<ul>
+		<a href="#">
+		<li>about us</li></a>
+		<a href="www.facebook.com/">
+		<li>facbook</li></a>
+		<a href="www.instagram.com">
+		<li>insta</li></a>
+		</div>
+		<div class="footer-section contact-form">
+		<h2>Contact</h2>
+		<p>nepaair@gmail.com</p>
+		</div>
+	<div class="footer-bottom">
+		&copy; nepaair.com.np | Design by nepaair
+	<div>
+	</div>
