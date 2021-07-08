10f0a65bfd0e4ea856ae7aa2d95fe6b17a43670b
Ashwin <ashwindg15@gmail.com>
2021-07-08 18:31:57 +0545

    Update login.php

--- login.php
+++ login.php
@@ -26,7 +26,7 @@ include('partials/navbar.php');
               <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
                 <div class="card card-signin my-5">
                   <div class="card-body">
-                    <h5 class="card-title text-center">Sign In</h5>
+                    <h5 class="card-title text-center">Log In</h5>
                     <form class="form-signin">
                       <div class="form-label-group">
                         <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
