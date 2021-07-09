d3c7e33f648e06c783255cfedd9a51604ca67ba8
Ashwin <ashwindg15@gmail.com>
2021-07-09 19:39:54 +0545

    update

--- admin/loginadmin.php
+++ admin/loginadmin.php
@@ -7,7 +7,7 @@
 "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<link rel="stylesheet" href="../partials/adminlogincss.css">
+	<link rel="stylesheet" href="partials/adminlogincss.css">
 	<title>Login Page</title>
 </head>
 
