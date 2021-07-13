383a0ea2e42ff32fc85284691c537657db97d4a0
Ashwin <ashwindg15@gmail.com>
2021-07-13 18:52:30 +0545

    Update adminindex.php

--- adminindex.php
+++ adminindex.php
@@ -4,67 +4,7 @@
 include('partials/adminnavbar.php');
 
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <link rel="shortcut icon" href="includes/img/favicon.ico" />
-        <link rel="stylesheet" type="text/css" href="includes/style1.css?<?php echo time(); ?>" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    	<title>Khwopa Airways</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <style>
-        body{
-    background-image:   url("includes/img/admin.jpg");
-    height: 110vh; 
-    background-position: center;
-    background-size: cover;
-   
-    margin: 0px;
- }
- header{
-     
-width: 100%;
-background-color: rgba(0,0, 0, .1);
-padding-left:-20px
- }
- </style>
-    </head>
-<body>
-    <header>
-        <div class="header">
-            
-                <img src="includes/img/logo.png" style="width:150px; border-radius:70%; height:145px; margin-top: 5px; margin-bottom: 5px ;" alt="Flight Template">
-                <ul id="right">
-                    
-                <li><a href="availableflights.php">Available Flights</li></a>
-                        <li><a href="deleteflights.php">Delete Flights</li></a>
-                        <li><a href="viewusers.php">View Users</li></a>
-                        <li><a href="viewbookedflights.php">View Booked Flights</li></a>
-                      
-                     
-                        <?php
-                      
-                        include("includes/connection.php");
-                        include("includes/functions.php");
-                        if(isset($_POST['admin'])){
-                        $userinfo=$_POST['user_name'];
-                        echo "<p style='text-align:right;''>Logged in as:<b>$userinfo</b></p>";
-                        }
-                       
-                        ?>
-                        <p style="text-align:right;"><a href="logout.php">Logout</a></p>
-</ul>
-                        
-                        
-                        <br>
-                         <br>
-                        
-            </ul>
-          </div>
-        </header>
+
         <div class="fieldset">
                 <center>
                 <h2 text-align="center">Add flights:</h2>
@@ -157,43 +97,9 @@ padding-left:-20px
 </form>
                               </center>
             </div>    
-          <footer class="footer-distributed">
-                               <div class="footer-left">
-                    <h3>Get to know us more</h3>
-                        <li><a href="#">About Us </li></a>
-                        <li><a href="#">Careers </li></a>
-                        <li><a href="#">Cargo </li></a>
-                        <li><a href="#">Site Map </li></a>
-                        <li><a href="#">FAQ </li></a>
-</div>
-                        <div class="footer-center">
-                    <h3>Legal Policies</h3>
-                        <li><a href="#">Privacy Policy </li></a>
-                        <li><a href="#">Insurance Policy </li></a>
-                        <li><a href="#">Terms and Condition </li></a>
-</div>
-                
-                        <div class="footer-right">
-                    <h3>Contact Us</h3>
-                        <li>Tel:01-4464928,01-0572631 </li>
-                        <li>Libali-7,Bhaktapur,Nepal </li>
-                        <li>P.O Box 44800 </li>
-                    </div>
-                    <div class="footer-icons">
- <h3>Follow Us On:</h3>
-		<a href="www.facebook.com/adhaugoda" ><i class="fa fa-facebook"></i></a>
-		<a href="#"><i class="fa fa-twitter"></i></a>
-		<a href="#"><i class="fa fa-instagram"></i></a>
-		<a href="#"><i class="fa fa-snapchat"></i></a>
- 
-		</div>
-                    <div class="mid-footer">
-                    ©2021 Khwopa Airways Pvt.Ltd.All Rights Reserved
-                    </div>
-                    
-                    
-                  
-                    </footer>
+          
+            <?php
 
-                    </body>
-</html> 
+include('partials/footer.php');
+
+?>
