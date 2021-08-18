ea6cae3352281c29000b2aace53c53f313991c15
Ashwin <ashwindg15@gmail.com>
2021-08-18 19:53:33 +0545

    halka css matra changed

--- adminindex.php
+++ adminindex.php
@@ -4,13 +4,17 @@
 include('partials/adminnavbar.php');
 
 ?>
+
 <link rel="stylesheet" href="partials/footer.css" />
-<link rel="stylesheet" href="partials/style.css" type="">
-        <div class="fieldset">
-                <center>
-                <h2 text-align="center">Add flights:</h2>
+
+<link rel="stylesheet" href="style.css" type="">
+              <div class="container ">
+                <h2>Add flights:</h2>
+                <div class="booking-formadmin">
                 <form id="form-submit" action="addflight.php" method="POST">
-                        <fieldset>
+                <div class="form-group">
+                <div class="row ">
+                <div class="col-md-3">
                                 <label for="from">From:</label>
                                 <select required name='from'>
                                     <option value="">Select a location...</option>
@@ -24,9 +28,10 @@ include('partials/adminnavbar.php');
                                     <option value="Nepalgunj">Nepalgunj</option>
                                     <option value="Chitwan">Chitwan</option>
                                     <option value="Jomsom ">Jomsom</option>
-                                </select>
-                            </fieldset>
-                            <fieldset>
+                                </select>       
+                                </div>
+                <div class="col-md-3">
+                            
                                 <label for="to">To:</label>
                                 <select required name='to' >
                                     <option value="">Select a location...</option>
@@ -41,16 +46,19 @@ include('partials/adminnavbar.php');
                                     <option value="Chitwan">Chitwan</option>
                                     <option value="Jomsom">Jomsom</option>
                                 </select>
-                            </fieldset>
-                                <fieldset>
+                                </div>
+</div>
+<div class="row">
+                <div class="col-md-3">
                                 <label for="departure" >Departure date:</label>
                                 <input name="departure" type="date" class="form-control date" id="deparure" placeholder="Select date..." required="" onchange='this.form.()'>
-                            </fieldset>
-                                <fieldset>
+                                </div>
+                <div class="col-md-3">
                                 <label for="return">Return date:</label>
                                 <input name="return" type="date" class="form-control date" id="return" placeholder="Select date..." required="" onchange='this.form.()'>
-                            </fieldset>
-                            <fieldset>
+                                </div></div>
+                                <div class="row">
+                <div class="col-md-3">
                                 <label for="seats">No.of seats</label>
                                 <select required name='seats' >
                                     <option value="">Select number of seats....</option>
@@ -58,7 +66,8 @@ include('partials/adminnavbar.php');
                                     <option value="30">30</option>
                                     <option value="60">60</option>
                                     <option value="90">90</option>
-                                    </select>
+                                    </select> </div>
+                <div class="col-md-3">
                                     <label for="cabin">Cabin:</label>
                                     <select required name="cabin" >
                                     <option value="">Select Cabin...</option>
@@ -67,7 +76,8 @@ include('partials/adminnavbar.php');
                                     <option value="Business">Business</option>
                                     <option value="First Class">First Class</option>
                                     </select>
-                                    
+                                    </div>
+                <div class="col-md-3">
                                     <select required name='price' >
                                     <option value="">Price Per Ticket</option>
                                     <option value="Rs.2000">Rs2000</option>
@@ -75,29 +85,39 @@ include('partials/adminnavbar.php');
                                     <option value="Rs.6000">Rs6000</option>
                                     <option value="Rs.10000">Rs10000</option>
                                     </select>
-                                    </fieldset>
-                            <fieldset>                            
+                                    </div></div>
+                                    <div class="row">
+                <div class="col-md-3">               
                                    <label for="round" >Round</label>
                                      <input type="radio" name="trip" id="round" value="round" required="required">
-                                    
+                                     </div>
+                <div class="col-md-3">
                                         <label for="oneway">Oneway</label>
                                         <input type="radio" name="trip" id="oneway" value="one-way" required="required">
                             
-                     
+                                        </div></div>
+                                        <div class="row">
+                <div class="col-md-3">
                                     
                                 <label for="departuretime" >Departure time:</label>
                                 <input name="departuretime" type="time" class="form-control date" id="departure" placeholder="Select time for departure..." required>
                         
-                          
+                                </div>
+                <div class="col-md-3">
                                 <label for="returntime">Return time:</label>
                                 <input name="returntime" type="time" class="form-control date" id="returntime" placeholder="Select return time..." required>
-                            </fieldset>
-                            
+                         
+                                </div>
+</div>
+<dv class="row">
+               <div class="col"><div class="form-btn">
+									<button   class="btn btn-primary btn-md" name="submit">Add to Database</button>
                                
-                                <input name="submit" type="submit" id="form-submit" value="Add flights to Database"  class="btn"></input>
-</form>
-                              </center>
-            </div>    
+</div></div>
+</div>   </div> 
+</form></div>
+                            
+            
           
             <?php
 
--- searchflights.php
+++ searchflights.php
@@ -24,29 +24,53 @@ if (isset($_POST['submit'])) {
 //	$seatclass = $_POST['cabin'];
    // $noofpassenger=$_POST['guests'];
   //  $_SESSION['nop']= $noofpassenger;
+ 
+  
+
     
     	$query = "SELECT * FROM availableflights WHERE  fro_m='$fromAirport' AND t_o='$toAirport' AND departur_e = '$Depart' AND retur_n='$Return' ";
         $result=mysqli_query($conn,$query);
         if(mysqli_num_rows($result)>0){
-            echo "Showing Flights Available For <b>$fromAirport</b> to <b>$toAirport</b>:";
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
+            echo "<br>";
+            echo "<div class='container'>";
+           
+             echo "Showing Flights Available For <b>$fromAirport</b> to <b>$toAirport</b>:";
+       echo "<table class='table table-hover table-bordered'><tr class='table table-primary'><th>From</th><th>To</th><th>Departure Date</th><th>Return Date</th><th>Cabin</th><th>Seats available</th><th>Trip Type</th><th>Price Per Ticket</th><th>Book Here<svg xmlns='http://www.w3.org/2000/svg' width='16' height=''16' fill='currentColor' class='bi bi-arrow-down-circle-fill' viewBox='0 0 16 16'>
+       <path d='M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z'/>
+     </svg></th></tr>";
+       
         	while(($row = mysqli_fetch_row($result))!=null)
 	{   
        $row[8]=$row[8]-$row[5];
-       echo "<table border = '2' style='border-collapse:collapse;'><tr><th>From</th><th>To</th><th>Departure Date</th><th>Return Date</th><th>Cabin</th><th>Seats available</th><th>Trip Type</th><th>Price Per Ticket</th><th></th></tr>";
-       
-        echo "<tr>";
+     
+       echo "<tr class='table table-secondary'>";
+        
         
     echo "<td>" .$row[1] ."</td>";
-    echo "<td>" .$row[2] ."</td>";    echo "<td>" .$row[3] ."</td>"; echo "<td>" .$row[4] ."</td>"; /*echo "<td>" .$row[] ."</td>";*/echo "<td>" .$row[6] ."</td>";  echo "<td>" .$row[8] ."</td>";echo "<td>" .$row[7] ."</td>"; echo "<td>" .$row[10] ."</td>"; echo"<td><a href='login.php?id=$row[0]' style='text-decoration:none; color:black;'>Book Flight</a></td>";
+    echo "<td>" .$row[2] ."</td>";    echo "<td>" .$row[3] ."</td>"; echo "<td>" .$row[4] ."</td>"; /*echo "<td>" .$row[] ."</td>";*/echo "<td>" .$row[6] ."</td>";  echo "<td>" .$row[8] ."</td>";echo "<td>" .$row[7] ."</td>"; echo "<td>" .$row[10] ."</td>"; echo"<td><a href='login.php?id=$row[0]' style='text-decoration:none; color:Black;'>Book Flight</a></td>";
     echo "</tr>";
-    echo "</table>";  
+    
    
     
     }
+    echo "</table>";
+    echo "</div>";
+    echo "</div>";  
+
+    echo "</div>";  
+
+
 }
-else {
+else {echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<center>";
     echo("<b>We are sorry! We do not have any  flights for this route at the moment.</b>");  echo "<br>";
-    
+    echo "</center>";
    }
 
 } 
