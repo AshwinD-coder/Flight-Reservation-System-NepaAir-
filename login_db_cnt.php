f9698d3041ae0bbd32d034e97a097202f3b7d4b8
Ashwin <ashwindg15@gmail.com>
2021-08-20 17:28:43 +0545

    motherfucker

--- login_db_cnt.php
+++ login_db_cnt.php
@@ -13,7 +13,7 @@ if(isset($_POST['submit'])){
 	$phone = $_POST['phone'];
     $nid=$_POST['nid-number'];
 	$_usernamenumber="Username Cannot be Number!";
-	$_success="Account Created Successfully!";
+	$_success="Account Created Successfully! Please Verify Your Email Address";
 	$_usernametaken="Username already taken!Try another!";
 	$_incorrectpassword="Incorrect Password!";
 	$ch="SELECT username FROM users WHERE username='$username'";
@@ -22,6 +22,22 @@ if(isset($_POST['submit'])){
 	// 	echo "<script>alert('Password must be upto 8 characters'); window.location='signup.php'</script>";    
 	// 	die;
 	// }
+	
+	//Sanitize form data
+// $fullname=$mysqli->real_escape_string($fullname);
+// $username=$mysqli->real_escape_string($username);
+// $password=$mysqli->real_escape_string($password);
+// $address=$mysqli->real_escape_string($address);
+// $presentaddress=$mysqli->real_escape_string($presentaddress);
+// $nid=$mysqli->real_escape_string($nid);
+// $email=$mysqli->real_escape_string($email);
+// $phone=$mysqli->real_escape_string($phone);
+// $country=$mysqli->real_escape_string($country);
+
+//Generate Vkey
+
+$vkey=md5(time().$username);
+
 		if(!is_numeric($username))
 		{
 			$sql=mysqli_query($conn, "select * from users WHERE username='$username'") or die (mysqli_error("Not conn"));
@@ -41,9 +57,22 @@ if(isset($_POST['submit'])){
 
     
 	
-				mysqli_query($conn, "INSERT INTO users (fullname, email, username, number, presentaddress, country, permnentaddress, NID, password) VALUES ('$fullname','$email','$username',$phone,'$presentaddress','$country','$address',$nid,'$enc')");
+				$insert=mysqli_query($conn, "INSERT INTO users (fullname, email, username, number, presentaddress, country, permnentaddress, NID, password,vkey) VALUES ('$fullname','$email','$username',$phone,'$presentaddress','$country','$address',$nid,'$enc','$vkey')");
 				$_SESSION['value']=$_success;
+				if($insert)
+				{
+					$to=$email;
+					$subject="Email Verification";
+					$message="<a href='https://localhost/fosp/Github/Project5Sem/verify.php?vkey=$vkey'>Register Account</a>";
+					$headers="From: nepairltd@gmail.com \r\n";
+					$headers .="MIME-Version: 1.0"."\r\n";
+					$headers .="Content-type:text/html;charset=UTF-8"."\r\n";
+					mail($to,$subject,$message,$headers);
 				
+				}
+				else{
+					echo $mysqli->error;
+				}
 			echo " <script>window.location='successsignup.php'</script>";
 		   
 			}
---
+++ verify.php
@@ -0,0 +1,23 @@
+<?php
+include("connection.php");
+if(isset($_GET['vkey'])){
+//Process Verification
+$vkey=$_GET['vkey'];
+$resultSet=mysqli_query($conn,"SELECT verified,vkey FROM users WHERE verified 0 AND vkey='$vkey' LIMIT 1");
+if(mysqli_num_rows($resultSet) ==1)
+{
+    //validate
+    $update=mysqli_query($conn,"UPDATE users SET verified = 1 where vkey='$vkey' LIMIT 1");
+    if($update)
+    {echo "Your Account has been verified.You may now login..";
+    }else
+    {echo $mysqli_error;
+}}
+else{
+    echo "This account is invalid or already verified";
+}
+}
+else{
+    die('Something went wrong.');
+}
+?>
