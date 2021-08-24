346f539dcf2a59e703ce304914b17ebb729bd039
Ashwin <ashwindg15@gmail.com>
2021-08-24 19:24:39 +0545

    asd

---
+++ esewa/setting.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Change Info From Here
+
+$epay_url = "https://uat.esewa.com.np/epay/main";
+$pid = "69123";
+$successurl = "https://localhost/fosp/EsewaWeb/success.php?q=su";
+$failedurl = "https://localhost/fosp/EsewaWeb/failed.php?q=fu";
+$merchant_code = "epay_payment"; 
+$fraudcheck_url = "https://uat.esewa.com.np/epay/transrec";
+
+// For Amount Check
+$actualamount = 1000;
+
+?>
