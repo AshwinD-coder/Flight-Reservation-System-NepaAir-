5a4aa74c1883d302b2e940db25c2b3915c11b4d0
Ashwin <ashwindg15@gmail.com>
2021-11-23 12:09:11 +0545

    dad

--- seat.php
+++ seat.php
@@ -2,23 +2,29 @@
 <?php
 
 include('partials/navbar.php');
+$pass=$_SESSION['nop'];
 ?>
 <head><script>
-  let counter=0;
+  let counter=<?php echo $pass;?>;
     
-  function count(){
-    // var count=document.getElementsByTagName('input');
+  function count(){  
+    var checkedValue = document.querySelectorAll('.seats:checked').value;
+    counter--;
+   
+   if(counter==0)
+   {
    
 
-    counter++;
-   if(counter==2)
-   {
     var x=document.getElementsByTagName('input');
   //  x.disabled=false;
-  for(var i=3;i<x.length;i++){
-   x[i].setAttribute("disabled", true);
+  for(var i=0;i<60;i++){
+   x[i].setAttribute("disabled", false);
+   
   }
+
    }
+
+   console.log(checkedValue);
    console.log("in");
     console.log(counter);
 
@@ -43,27 +49,27 @@ include('partials/navbar.php');
 
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="1A" disabled/ >
+          <input type="checkbox" id="1A" onClick="count()" value="1A" class="seats" >
           <label for="1A">1A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="1B" onClick="count()" />
+          <input type="checkbox" id="1B" onClick="count()" value="1B"  class="seats" />
           <label for="1B">1B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="1C" onClick="count()" />
+          <input type="checkbox" id="1C" onClick="count()" value="1C" class="seats"  />
           <label for="1C">1C</label>
         </li>
         <li class="seat">
-          <input type="checkbox"  id="1D" class="dis" />
+          <input type="checkbox"  id="1D" class="dis" onClick="count()"/>
           <label for="1D">1D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="1E" class="dis"/>
+          <input type="checkbox" id="1E" class="dis" onClick="count()"/>
           <label for="1E">1E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="1F" />
+          <input type="checkbox" id="1F" onClick="count()"/>
           <label for="1F">1F</label>
         </li>
       </ol>
@@ -71,27 +77,27 @@ include('partials/navbar.php');
     <li class="row row--2">
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="2A" />
+          <input type="checkbox" id="2A" onClick="count()" />
           <label for="2A">2A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="2B" />
+          <input type="checkbox" id="2B" onClick="count()" />
           <label for="2B">2B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="2C" />
+          <input type="checkbox" id="2C"  onClick="count()"/>
           <label for="2C">2C</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="2D" />
+          <input type="checkbox" id="2D" onClick="count()" />
           <label for="2D">2D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="2E" />
+          <input type="checkbox" id="2E" onClick="count()" />
           <label for="2E">2E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="2F" />
+          <input type="checkbox" id="2F" onClick="count()" />
           <label for="2F">2F</label>
         </li>
       </ol>
@@ -99,27 +105,27 @@ include('partials/navbar.php');
     <li class="row row--3">
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="3A" />
+          <input type="checkbox" id="3A" onClick="count()" />
           <label for="3A">3A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="3B" />
+          <input type="checkbox" id="3B" onClick="count()"/>
           <label for="3B">3B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="3C" />
+          <input type="checkbox" id="3C" onClick="count()" />
           <label for="3C">3C</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="3D" />
+          <input type="checkbox" id="3D" onClick="count()" />
           <label for="3D">3D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="3E" />
+          <input type="checkbox" id="3E" onClick="count()" />
           <label for="3E">3E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="3F" />
+          <input type="checkbox" id="3F" onClick="count()"/>
           <label for="3F">3F</label>
         </li>
       </ol>
@@ -127,27 +133,27 @@ include('partials/navbar.php');
     <li class="row row--4">
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="4A" />
+          <input type="checkbox" id="4A" onClick="count()"/>
           <label for="4A">4A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="4B" />
+          <input type="checkbox" id="4B" onClick="count()" />
           <label for="4B">4B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="4C" />
+          <input type="checkbox" id="4C" onClick="count()" />
           <label for="4C">4C</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="4D" />
+          <input type="checkbox" id="4D" onClick="count()" />
           <label for="4D">4D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="4E" />
+          <input type="checkbox" id="4E"  onClick="count()"/>
           <label for="4E">4E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="4F" />
+          <input type="checkbox" id="4F"  onClick="count()"/>
           <label for="4F">4F</label>
         </li>
       </ol>
@@ -155,27 +161,27 @@ include('partials/navbar.php');
     <li class="row row--5">
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="5A" />
+          <input type="checkbox" id="5A" onClick="count()"/>
           <label for="5A">5A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="5B" />
+          <input type="checkbox" id="5B" onClick="count()" />
           <label for="5B">5B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="5C" />
+          <input type="checkbox" id="5C"  onClick="count()"/>
           <label for="5C">5C</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="5D" />
+          <input type="checkbox" id="5D" onClick="count()" />
           <label for="5D">5D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="5E" />
+          <input type="checkbox" id="5E"  onClick="count()"/>
           <label for="5E">5E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="5F" />
+          <input type="checkbox" id="5F"  onClick="count()"/>
           <label for="5F">5F</label>
         </li>
       </ol>
@@ -183,27 +189,27 @@ include('partials/navbar.php');
     <li class="row row--6">
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="6A" />
+          <input type="checkbox" id="6A"  onClick="count()"/>
           <label for="6A">6A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="6B" />
+          <input type="checkbox" id="6B" onClick="count()" />
           <label for="6B">6B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="6C" />
+          <input type="checkbox" id="6C" onClick="count()" />
           <label for="6C">6C</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="6D" />
+          <input type="checkbox" id="6D" onClick="count()" />
           <label for="6D">6D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="6E" />
+          <input type="checkbox" id="6E" onClick="count()" />
           <label for="6E">6E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="6F" />
+          <input type="checkbox" id="6F"  onClick="count()"/>
           <label for="6F">6F</label>
         </li>
       </ol>
@@ -211,27 +217,27 @@ include('partials/navbar.php');
     <li class="row row--7">
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="7A" />
+          <input type="checkbox" id="7A" onClick="count()" />
           <label for="7A">7A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="7B" />
+          <input type="checkbox" id="7B"  onClick="count()"/>
           <label for="7B">7B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="7C" />
+          <input type="checkbox" id="7C"  onClick="count()"/>
           <label for="7C">7C</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="7D" />
+          <input type="checkbox" id="7D"  onClick="count()"/>
           <label for="7D">7D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="7E" />
+          <input type="checkbox" id="7E" onClick="count()"/>
           <label for="7E">7E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="7F" />
+          <input type="checkbox" id="7F" onClick="count()" />
           <label for="7F">7F</label>
         </li>
       </ol>
@@ -239,27 +245,27 @@ include('partials/navbar.php');
     <li class="row row--8">
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="8A" />
+          <input type="checkbox" id="8A" onClick="count()" />
           <label for="8A">8A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="8B" />
+          <input type="checkbox" id="8B"  onClick="count()"/>
           <label for="8B">8B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="8C" />
+          <input type="checkbox" id="8C" onClick="count()" />
           <label for="8C">8C</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="8D" />
+          <input type="checkbox" id="8D"  onClick="count()"/>
           <label for="8D">8D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="8E" />
+          <input type="checkbox" id="8E" onClick="count()" />
           <label for="8E">8E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="8F" />
+          <input type="checkbox" id="8F"  onClick="count()"/>
           <label for="8F">8F</label>
         </li>
       </ol>
@@ -267,27 +273,27 @@ include('partials/navbar.php');
     <li class="row row--9">
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="9A" />
+          <input type="checkbox" id="9A"  onClick="count()"/>
           <label for="9A">9A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="9B" />
+          <input type="checkbox" id="9B"  onClick="count()"/>
           <label for="9B">9B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="9C" />
+          <input type="checkbox" id="9C" onClick="count()" />
           <label for="9C">9C</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="9D" />
+          <input type="checkbox" id="9D" onClick="count()" />
           <label for="9D">9D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="9E" />
+          <input type="checkbox" id="9E"  onClick="count()"/>
           <label for="9E">9E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="9F" />
+          <input type="checkbox" id="9F" onClick="count()" />
           <label for="9F">9F</label>
         </li>
       </ol>
@@ -295,27 +301,27 @@ include('partials/navbar.php');
     <li class="row row--10">
       <ol class="seats" type="A">
         <li class="seat">
-          <input type="checkbox" id="10A" />
+          <input type="checkbox" id="10A" onClick="count()" />
           <label for="10A">10A</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="10B" />
+          <input type="checkbox" id="10B" onClick="count()" />
           <label for="10B">10B</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="10C" />
+          <input type="checkbox" id="10C" onClick="count()" />
           <label for="10C">10C</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="10D" />
+          <input type="checkbox" id="10D"  onClick="count()"/>
           <label for="10D">10D</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="10E" />
+          <input type="checkbox" id="10E" onClick="count()" />
           <label for="10E">10E</label>
         </li>
         <li class="seat">
-          <input type="checkbox" id="10F" />
+          <input type="checkbox" id="10F"  onClick="count()"/>
           <label for="10F">10F</label>
         </li>
       </ol>
