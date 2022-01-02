341203e3d50d2236d2a00c052a29761e9c49f677
Ashwin <ashwindg15@gmail.com>
2022-01-02 20:08:35 +0545

    cancel

--- cancel.php
+++ cancel.php
@@ -1,10 +1,66 @@
 <?php
 
 include('partials/navbar.php');
+$user=$_SESSION['username'];
+if(isset($_GET['id']))
+{
+  $id=$_GET['id'];
+  
+}
+if(isset($_GET['cancel']))
+{
+    $id=$_GET['cancel'];
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    
+    $query="UPDATE contact set BookingStatus='Cancelled' WHERE user_name='$user' AND FLightNO='$id'";
+   if(mysqli_query($conn,$query))
+   {
+    date_default_timezone_set("Asia/Kathmandu");
+    $sDate=date("Y-m-d H:i:s");
+    $query2="UPDATE contact set CancelledTime='$sDate' WHERE user_name='$user' AND FLightNO='$id'";
+    $y=mysqli_query($conn,$query2);
+    $trim= trim($id,'NA-');
+    $query3="SELECT priceperticket FROM availableflights WHERE id='$trim'";
+    $price=mysqli_query($conn,$query3);
+    $result=mysqli_query($conn,"SELECT * FROM availableflights WHERE id='$trim'");
+    $row = mysqli_fetch_row($result);
+    $dt=$row[3].' '.$row[9].':00';
+    $dt2=mysqli_query($conn,"SELECT CancelledTime FROM contact WHERE user_name='$user' AND FlightNO='$id'");
+$dt2 = $dt2 -> fetch_assoc();
+$price = $price -> fetch_assoc();
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo $dt;
+    echo "<br>";
+    
+    $dt=strtotime($dt);
+    $dt2=strtotime($dt2['CancelledTime']);
+    $try=$dt-$dt2;
+    echo $try;
+    
+    
+    if($try<=7200)
+    {
+       echo "<script> alert('Flight cannot be cancelled before 2 hrs of the flight.');</script>;";
+    }
+    if($try<39600){
+        echo "<script> alert('Flight  cancelled within 11 hrs of the flight.33% fare is levied.');</script>;";
 
+    }
+    if($try>39600){
+        echo "<script> alert('Flight  cancelled before 11 hrs of the flight.11% fare is levied.');</script>;";
 
+    }
+  
+  
+
+  
+}}
 ?>
-
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
  
@@ -14,6 +70,12 @@ include('partials/navbar.php');
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="partials/style.css?<?php echo time(); ?>" type=""><br><br><br><br>
 <head>
+<script type="text/javascript">
+    $(window).on('load', function() {
+        $('#myModal').modal('show');
+    });
+</script>
+
     <style>
         .container li{
             font-size:large;
@@ -41,7 +103,17 @@ Refunds are made from the point of issue.</li>
 
 </ul>
 </div>
-<div class="col-md-6">
-    <h2>Do you want to cancel the flight?</h2>;
-
+<div class="container">
+    <h2>Do you want to cancel the flight?</h2>
+    <div class=" btn col-md-1">
+    <a href="cancel.php?cancel=<?php echo $id?>"><button type='button' class='btn btn-danger' >
+  Proceed 
+    </button></a>
 </div>
+    
+<div class="btn col-md-1">
+    <a href="flightinfo.php"><button type='button' class='btn btn-success' >
+  Skip 
+    </button></a>
+</div>
+    
--- flightinfo.php
+++ flightinfo.php
@@ -5,15 +5,13 @@ include('partials/navbar.php');
 
 ?>
 
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
-
- 
- 
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"> 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="partials/style.css?<?php echo time(); ?>" type="">
 <head>
+
     <style>
         .heading{
             font-family: "Garamond", serif;
@@ -61,7 +59,20 @@ $result=mysqli_query($conn,$query);
     $result2=mysqli_query($conn,$query2);
     if(mysqli_num_rows($result2)>0){
         while(($row2 = mysqli_fetch_row($result2))!=null)
-{   echo "<td> $row2[1] </td><td> $row2[2] </td><td> $row2[3] </td><td> $row2[9] </td><td> $row[12] </td><td> $row2[5] </td><td>$row[13]</td><td></td><td><a style='text-decoration:none; color:white;' href='cancel.php'>Cancel flight</a>";
+{   echo "<td> $row2[1] </td><td> $row2[2] </td><td> $row2[3] </td><td> $row2[9] </td><td> $row[12] </td><td> $row2[5] </td><td>$row[13]</td><td></td>";
+    $id=$row[11];
+  
+    if($row[0]=='Booked')
+    {echo "<td><a href='cancel.php?id=$id'><button type='button' class='btn btn-danger' data-toggle='modal' data-target='#exampleModalCenter'>
+   Cancel Flight
+  </button></a></td>";
+}
+   if($row[0]=='Cancelled'){
+  echo "<td><button type='success' class='btn btn-success' data-toggle='modal' data-target='#exampleModalCenter'>
+  Reschedule flight
+ </button></td>";
+    }
+
 }}}}
 else{
     echo "Please book a flight to view you flight information.";
