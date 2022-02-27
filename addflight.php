d9d7c945c248e81c5047306996f810f3b3bda3f8
Ashwin <ashwindg15@gmail.com>
2022-02-27 23:23:31 +0545

    xD

--- addflight.php
+++ addflight.php
@@ -1,29 +1,28 @@
 
-<?php 
-  session_start();
-  include("connection.php");
-  
-
-if(isset($_POST['submit'])){
-$from=$_POST['from'];
-$to=$_POST['to'];
-$departure=$_POST['departure'];
-
-$seats=$_POST['seats'];
-$cabin=$_POST['cabin'];
-
-$book='Not Booked';
-$price=$_POST['price'];
-$deptime=$_POST['departuretime'];
+<?php
+session_start();
+include("connection.php");
 
 
-mysqli_query($conn, "INSERT INTO availableflights(Fro_m,t_o,departur_e,cabi_n,totalseats,Status,priceperticket,departuretime)
+if (isset($_POST['submit'])) {
+  $from = $_POST['from'];
+  $to = $_POST['to'];
+  $departure = $_POST['departure'];
+
+  $seats = $_POST['seats'];
+  $cabin = $_POST['cabin'];
+
+  $book = 'Not Booked';
+  $price = $_POST['price'];
+  $deptime = $_POST['departuretime'];
+
+
+  mysqli_query($conn, "INSERT INTO availableflights(Fro_m,t_o,departur_e,cabi_n,totalseats,Status,priceperticket,departuretime)
 			    VALUES ('$from','$to','$departure','$cabin','$seats','$book','$price','$deptime')")
-                 or die (mysqli_error("Error"));
-			    echo "<script>alert('added to database'); window.location='adminindex.php'</script>";
-}
-else{
-    echo "<script>alert('Not added to database'); window.location='adminindex.php'</script>";
+    or die(mysqli_error($conn));
+  echo "<script>alert('added to database'); window.location='adminindex.php'</script>";
+} else {
+  echo "<script>alert('Not added to database'); window.location='adminindex.php'</script>";
 }
 ?>
 
--- checkseat.php
+++ checkseat.php
@@ -1,4 +1,5 @@
 <br><br><br><br>
+
 <?php
 
 include('partials/navbar.php');
@@ -9,27 +10,27 @@ $id= $_SESSION['id'];
 $k="NA-";
 $id=$k.$id;
 $trim= trim($id,'NA-');
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
+echo "<div style='text-align:center;'><img src='partials/loading.gif' alt='loading'  height='300' align></div>";
+
 echo "<Center><b>Completing final entries and finalizing booking....................</b></center>";
 
 if(isset($_POST['form']));
 {$select= $_POST['seat'];
     $select = str_replace(',', ' ', $select);
-}
+
 $string_length = strlen($select) - substr_count($select, ' ');
 $length=$string_length/2;
+}
 if($length>$pass)
 {
-    echo "<script>alert('Please Select seats for $pass passengers!');window.location='seat.php';</script>";
+    echo "<script>alert('Please Select seats for $pass passengers!');
+    window.location='seat.php';</script>";
 }
-if($length<$pass)
+else if($length<$pass)
 {
     echo "<script>alert('Please select seats for $pass passengers!');window.location='seat.php';</script>";
 }
-if($length==$pass)
+else if($length==$pass)
 {
     mysqli_query($conn,"UPDATE contact SET seats='$select' WHERE user_name='$user' AND FlightNO='$id'");
     $result=mysqli_query($conn,"SELECT Seatsbooked FROM availableflights WHERE id='$trim'");
@@ -38,8 +39,16 @@ if($length==$pass)
     $result=$result.' '.$select;
     mysqli_query($conn,"UPDATE availableflights SET Seatsbooked='$result' WHERE  id='$trim'");
     
-    echo "<script>window.location='esewa/thankyou.php';</script>";
+    echo "<script>
+    setTimeout(function(){
+        window.location.href = 'esewa/thankyou.php';;
+     }, 2000);</script>";
 
+}
+
+else
+{
+    die(mysqli_error($_POST['form']));
 }
 
 ?>
