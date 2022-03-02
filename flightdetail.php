c7a9be4b5ae375c285954e4c99be44c15f820402
AshishLawaju <ashish.lawaju74@gmail.com>
2022-03-02 14:51:19 +0545

    ok xa ta

--- flightdetail.php
+++ flightdetail.php
@@ -13,12 +13,42 @@ include('partials/navbar.php');
 <link rel="stylesheet" href="partials/footer.css?<?php echo time(); ?>" type="">
 
 
-
-
-
 <?php
+echo "<br>";
+echo "<br>";
+echo "<br>";
 
-include('partials/footer.php');
+echo "<tr class='table table-secondary'>";
+echo "<div class='container'>";
+        echo "<table class='table table-hover table-bordered'><tr class='table table-primary'><th>Flight No</th><th>From</th><th>To</th><th>Departure Date</th><th>Departure Time</th><th>Cabin</th><th>Seats available</th><th>PassengerCount</th><th>Price Per Ticket</th><th>Status</th>";
+$query="SELECt * FROM availableflights";
+
+$result = mysqli_query($conn,$query);
+
+while (($row = mysqli_fetch_row($result)) != null){
+echo "<tr>";
+echo "<td>NA-" . $row[0] . "</td>";
+echo "<td>" . $row[1] . "</td>";
+echo "<td>" . $row[2] . "</td>";
+echo "<td>" . $row[3] . "</td>";
+echo "<td>" . $row[9] . "</td>";
+echo "<td>" . $row[5] . "</td>";
+echo "<td>" . $row[6] . "</td>";
+echo "<td>" . $row[4] . "</td>";
+
+echo "<td>" . $row[8] . "</td>";
+echo "<td>" . $row[7] . "</td>";
+//echo "<td>" . $row[10] . "</td>";
+echo "</tr>";
 
 
+}
+echo "</table>";
 ?>
+
+<!-- <?php
+
+//include('partials/footer.php');
+
+
+?> -->
