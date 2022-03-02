09d8c1895d30aa96de42169091bf92ff255ae435
AshishLawaju <ashish.lawaju74@gmail.com>
2022-03-02 15:37:15 +0545

    ok

--- flightdetail.php
+++ flightdetail.php
@@ -20,7 +20,7 @@ echo "<br>";
 
 echo "<tr class='table table-secondary'>";
 echo "<div class='container'>";
-        echo "<table class='table table-hover table-bordered'><tr class='table table-primary'><th>Flight No</th><th>From</th><th>To</th><th>Departure Date</th><th>Departure Time</th><th>Cabin</th><th>Seats available</th><th>PassengerCount</th><th>Price Per Ticket</th><th>Status</th>";
+        echo "<table class='table table-hover table-bordered'><tr class='table table-primary'><th>Flight No</th><th>From</th><th>To</th><th>Departure Date</th><th>Departure Time</th><th>Cabin</th><th>Price Per Ticket</th><th>Status</th>";
 $query="SELECt * FROM availableflights";
 
 $result = mysqli_query($conn,$query);
@@ -33,8 +33,8 @@ echo "<td>" . $row[2] . "</td>";
 echo "<td>" . $row[3] . "</td>";
 echo "<td>" . $row[9] . "</td>";
 echo "<td>" . $row[5] . "</td>";
-echo "<td>" . $row[6] . "</td>";
-echo "<td>" . $row[4] . "</td>";
+//echo "<td>" . $row[6] . "</td>";
+//echo "<td>" . $row[4] . "</td>";
 
 echo "<td>" . $row[8] . "</td>";
 echo "<td>" . $row[7] . "</td>";
